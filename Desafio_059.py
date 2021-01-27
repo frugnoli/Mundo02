de63277e0859c5eec9ca26aa7f61495de6115431
Marcello Frugnoli <frugnoli@outlook.com>
2021-01-27 18:01:17 -0300

    Update Desafio_059.py

--- Desafio_059.py
+++ Desafio_059.py
@@ -8,18 +8,35 @@
 from time import sleep
 primeiro = int(input("Primeiro Valor: "))
 segundo = int(input("Segundo Valor: "))
-print("""
-[ 1 ] somar
-[ 2 ] multiplicar
-[ 3 ] maior
-[ 4 ] novos números
-[ 5 ] sair do programa""")
-opcao = int(input(">>>>>> Qual é a sua opção: "))
+opcao = 0
 while opcao != 5:
+    print("""
+    [ 1 ] somar
+    [ 2 ] multiplicar
+    [ 3 ] maior
+    [ 4 ] novos números
+    [ 5 ] sair do programa
+    """)
+    opcao = int(input(">>>>>> Qual é a sua opção: "))
+    print("-=" * 20)
     if opcao == 1:
-        print("A soma dos resultados é {}".format(primeiro + segundo))
-        print("-=" * 20)
-print("Finalisando . . .")
-print("-=" * 20)
+        print("A soma dos valores é {}".format(primeiro + segundo))
+    elif opcao == 2:
+        print("A multiplicação dos valores é {}".format(primeiro * segundo))
+    elif opcao == 3:
+        if primeiro > segundo:
+            print("Primeiro valor é maior!")
+        elif segundo > primeiro:
+            print("Segundo valor é maior!")
+        else:
+            print("Valores iguais!")
+    elif opcao == 4:
+        primeiro = int(input("Primeiro Valor: "))
+        segundo = int(input("Segundo Valor: "))
+    elif opcao == 5:
+        print("Finalisando . . .")
+    else:
+        print("Opção Inválida!")
+    print("-=" * 20)
 sleep(1)
 print("FIM DO PROGRAMA, VOLTE SEMPRE!")
