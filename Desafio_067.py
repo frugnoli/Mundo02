37bd10923014d907ea56b0289fb9b73180a6b6b6
Marcello Frugnoli <frugnoli@outlook.com>
2021-02-03 19:20:39 -0300

    Update Desafio_067.py

--- Desafio_067.py
+++ Desafio_067.py
@@ -1,3 +1,15 @@
 # Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. 
 # O programa será interrompido quando o número solicitado for negativo.
+
+
+while True:
+    print("-" * 50)
+    num = int(input("Quer ver a tabuada de qual valor? "))
+    print("-" * 50)
+    if num < 0:
+        break
+    else:
+        for c in range(0, 11):
+            print(f"{num} x {c} = {num * c}") 
+print("PROGRAMA DE TABUADA ENCERRADO. VOLTE SEMPRE!\n")
  
