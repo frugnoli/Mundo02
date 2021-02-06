48dc08c5667c1f2957fae27a57c9b072c3cac151
Marcello Frugnoli <frugnoli@outlook.com>
2021-02-06 11:19:58 -0300

    Update Desafio_069.py

--- Desafio_069.py
+++ Desafio_069.py
@@ -4,6 +4,23 @@
 # A) quantas pessoas tem mais de 18 anos.
 # B) quantos homens foram cadastrados.
 # C) quantas mulheres tem menos de 20 anos.
-
+h = p = m = 0
 while True:
-    
+    idade = int(input("Idade: "))
+    if idade > 18:
+        p += 1
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input("Sexo: [M/F] ")).strip().upper()[0]
+        if sexo == 'M':
+            h += 1
+        if sexo == 'F' and idade < 20:
+            m += 1
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input("Quer continuar [S/N]: ")).strip().upper()[0]
+    if resp == 'N':
+        break
+print(f"Total de {p} pessoas com mais de 18 anos")
+print(f"Total de {h} homens cadastrados")
+print(f"Total de {m} mulheres com menos de 20 anos")  
